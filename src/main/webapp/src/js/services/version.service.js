60865fae7706a7e44928d4505ede8a9ff92e36b1
Fabiano Góes <fgoes@dellavolpe.com.br>
2017-03-30 17:24:32 -0300

    implemented increment version by build travis

--- src/main/webapp/src/js/services/version.service.js
+++ src/main/webapp/src/js/services/version.service.js
@@ -1,7 +1,7 @@
 appbruxelas.factory('VersionService', ['$http', function($http) {
 
     var _findActualVersion = function(userId) {
-        return $http.get('http://localhost:8080/bruxelas/api/version');
+        return $http.get('http://bruxelas-fabianoggoes.rhcloud.com/api/version');
     }
 
     return {
