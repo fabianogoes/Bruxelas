ca61319275141153893fa01ebcc157405ddf5290
Fabiano Góes <fgoes@dellavolpe.com.br>
2017-03-31 11:37:35 -0300

    configure HSQLDB in Profile test and MySQL in Profile prod

--- src/main/java/com/bruxelas/repositories/VersionRepository.java
+++ src/main/java/com/bruxelas/repositories/VersionRepository.java
@@ -10,7 +10,7 @@ import com.bruxelas.entities.Version;
 public interface VersionRepository extends CrudRepository<Version, Long> {
 
 	
-	@Query(value = "select * from Version v where v.id = (select max(vv.id) from Version vv)", nativeQuery = true)
+	@Query(value = "select * from version v where v.id = (select max(vv.id) from version vv)", nativeQuery = true)
 	Version findActualVersion();
 	
 }
